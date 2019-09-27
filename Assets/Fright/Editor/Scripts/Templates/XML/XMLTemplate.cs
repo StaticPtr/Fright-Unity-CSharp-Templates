c904cf0cce9243e9d38ae58764c8421cd3910a88
Brandon Dahn <brandond.mobile@gmail.com>
2019-09-27 15:56:03 -0700

    Added XmlTemplate

--- Assets/Fright/Editor/Scripts/Templates/XML/XMLTemplate.cs
+++ Assets/Fright/Editor/Scripts/Templates/XML/XMLTemplate.cs
@@ -1,16 +1,82 @@
-﻿using System.Collections;
+﻿using System.Xml;
+using System.Text;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
-public class XmlTemplate : MonoBehaviour {
+namespace Fright.Editor.Templates
+{
+	/// Describes a template made from Xml that can be used to make a C# file
+	public class XmlTemplate : XmlBase
+	{
+		private static Dictionary<string, System.Type> xmlBaseTypes = new Dictionary<string, System.Type>()
+		{
+			{"function", typeof(XmlFunction)},
+			{"class", typeof(XmlClass)},
+		};
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+		public List<XmlBase> children = new List<XmlBase>();
+
+		public override void ConstructFromXml(XmlNode node, XmlDocument document)
+		{
+			base.ConstructFromXml(node, document);
+
+			//Children
+			foreach(XmlNode child in node.ChildNodes)
+			{
+				XmlBase xmlBase = CreateXmlObjectFromNode(child, document);
+
+				if (xmlBase != null)
+				{
+					children.Add(xmlBase);
+				}
+			}
+		}
+
+		/// Converts the XML object into C# and adds it to the string builder
+		public override void ToCSharp(StringBuilder stringBuilder, int indentationLevel)
+		{
+			//Usings
+			//...
+
+			//Namespaces
+			//...
+
+			//Children
+			bool isFirstChild = true;
+
+			foreach(var child in children)
+			{
+				if (isFirstChild)
+				{
+					isFirstChild = false;
+				}
+				else
+				{
+					stringBuilder.Append("\n\n");
+				}
+
+				child.ToCSharp(stringBuilder, indentationLevel);
+			}
+		}
+
+		/// Constructs an XmlBase from the node if it's type is known
+		public static XmlBase CreateXmlObjectFromNode(XmlNode node, XmlDocument document)
+		{
+			XmlBase result = null;
+			System.Type nodeType = null;
+
+			//Instantiate the type if it's a known type
+			if (xmlBaseTypes.TryGetValue(node.LocalName.ToLower(), out nodeType) && nodeType != null)
+			{
+				if (typeof(XmlBase).IsAssignableFrom(nodeType) && !nodeType.IsAbstract)
+				{
+					result = System.Activator.CreateInstance(nodeType) as XmlBase;
+					result.ConstructFromXml(node, document);
+				}
+			}
+
+			//Return the result
+			return result;
+		}
 	}
 }
---
+++ Assets/Fright/Editor/Tests/TestXMLTemplates.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Text;
+using System.Xml;
+using System.Collections;
+using Fright.Editor.Templates;
+
+namespace Fright.Editor.Tests
+{
+	public class Templates
+	{
+		[TestCase("Complex")]
+		public void RunXmlToCodeTest(string id)
+		{
+			string xml = GetXml(id);
+			XmlDocument document = new XmlDocument();
+			document.LoadXml(xml);
+
+			XmlTemplate template = new XmlTemplate();
+			template.ConstructFromXml(document.GetFirstChild("template"), document);
+
+			string generatedCode = SerializeToCode(template);
+			string expectedCode = GetExpectedResult(id);
+			Assert.AreEqual(expectedCode, generatedCode);
+		}
+
+		private string SerializeToCode(XmlBase xmlBase)
+		{
+			StringBuilder generatedCode = new StringBuilder();
+			xmlBase.ToCSharp(generatedCode, 0);
+			return generatedCode.ToString().Replace("\r\n", "\n");
+		}
+
+		private string GetExpectedResult(string id)
+		{
+			return System.IO.File.ReadAllText("Assets/Fright/Editor/Tests/Test Expectations/Template_" + id);
+		}
+
+		private string GetXml(string id)
+		{
+			return System.IO.File.ReadAllText("Assets/Fright/Editor/Tests/Test XML/Template_" + id + ".xml");
+		}
+	}
+}
