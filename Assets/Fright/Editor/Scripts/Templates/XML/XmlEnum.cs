b4b664b9e6b92e29b50d6f5b2b9a3421ad262b95
Brandon Dahn <brandond.mobile@gmail.com>
2019-09-29 16:23:04 -0700

    Added enums and interfaces

---
+++ Assets/Fright/Editor/Scripts/Templates/XML/XmlEnum.cs
@@ -0,0 +1,31 @@
+﻿using System.Xml;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Fright.Editor.Templates
+{
+	/// Describes an enum that can be used in an XML Template
+	public class XmlEnum : XmlType
+	{
+		public override string kind
+		{
+			get { return "enum"; }
+		}
+
+		public override string xmlType
+		{
+			get { return "enum"; }
+		}
+
+		public override void ConstructFromXml(XmlNode node, XmlDocument document)
+		{
+			base.ConstructFromXml(node, document);
+			isSealed = false;
+			isStatic = false;
+			isAbstract = false;
+			isPartial = false;
+			interfaces.Clear();
+		}
+	}
+}
---
+++ Assets/Fright/Editor/Scripts/Templates/XML/XmlInterface.cs
@@ -0,0 +1,30 @@
+﻿using System.Xml;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Fright.Editor.Templates
+{
+	/// Describes an interface that can be used in an XML Template
+	public class XmlInterface : XmlType
+	{
+		public override string kind
+		{
+			get { return "interface"; }
+		}
+
+		public override string xmlType
+		{
+			get { return "interface"; }
+		}
+
+		public override void ConstructFromXml(XmlNode node, XmlDocument document)
+		{
+			base.ConstructFromXml(node, document);
+			isSealed = false;
+			isStatic = false;
+			isAbstract = false;
+			isPartial = false;
+		}
+	}
+}
