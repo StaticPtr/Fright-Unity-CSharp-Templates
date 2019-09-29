4b4334bcef1be2b0b2821f831de5afe69ea25e4a
Brandon Dahn <brandond.mobile@gmail.com>
2019-09-28 21:24:05 -0700

    Removed System.Serializable

--- Assets/Fright/Editor/Scripts/Templates/TemplateBuilderSettings.cs
+++ Assets/Fright/Editor/Scripts/Templates/TemplateBuilderSettings.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 namespace Fright.Editor.Templates
 {
-	[System.Serializable]
 	public class TemplateBuilderSettings
 	{
 		public TemplateBuilder.LineEndings lineEndings = TemplateBuilder.LineEndings.unix;
